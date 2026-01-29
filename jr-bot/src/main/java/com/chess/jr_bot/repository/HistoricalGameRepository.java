793846619a870934fe7b2e72101d6251f71b927c
Junior <josephson-junior.razafindratsiza@eleve.ensai.fr>
2026-01-29 21:13:53 +0100

    section Analyse prête

--- jr-bot/src/main/java/com/chess/jr_bot/repository/HistoricalGameRepository.java
+++ jr-bot/src/main/java/com/chess/jr_bot/repository/HistoricalGameRepository.java
@@ -1,11 +1,12 @@
 package com.chess.jr_bot.repository;
 
-import com.chess.jr_bot.entity.HistoricalGameEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.chess.jr_bot.entity.HistoricalGameEntity;
 
 public interface HistoricalGameRepository extends JpaRepository<HistoricalGameEntity, String> {
 
